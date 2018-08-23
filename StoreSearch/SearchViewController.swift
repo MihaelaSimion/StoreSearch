8b06a3db37d49ae731d26a696b2421bf52094c08
Mihaela Simion <mihaelasimion2016@gmail.com>
2018-08-23 13:22:19 +0300

    Set up the UI - added the table view and the search bar

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Mihaela Simion on 8/23/18.
+//  Copyright © 2018 Mihaela Simion. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
