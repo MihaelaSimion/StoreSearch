91c042ddfae87718993983fff97c0062a050fd7c
Mihaela Simion <mihaelasimion2016@gmail.com>
2018-08-23 16:03:20 +0300

    Create the SearchResult class to hold the data

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Mihaela Simion on 8/23/18.
+//  Copyright © 2018 Mihaela Simion. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
