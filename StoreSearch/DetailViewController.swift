060fa49de1d1f8acf667e4802e03f7b8d1930b2e
Mihaela Simion <mihaelasimion2016@gmail.com>
2018-08-27 14:59:03 +0300

    Add the DimmingPresentationController

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -12,24 +12,22 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        modalPresentationStyle = .custom
+        transitioningDelegate = self
     }
-    */
 
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
+
+extension DetailViewController: UIViewControllerTransitioningDelegate {
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        return DimmingPresentationController(presentedViewController: presented, presenting: presenting)
+    }
 }
---
+++ StoreSearch/DimmingPresentationController.swift
@@ -0,0 +1,15 @@
+//
+//  DimmingPresentationController.swift
+//  StoreSearch
+//
+//  Created by Mihaela Simion on 8/27/18.
+//  Copyright © 2018 Mihaela Simion. All rights reserved.
+//
+
+import UIKit
+
+class DimmingPresentationController: UIPresentationController {
+    override var shouldRemovePresentersView: Bool {
+        return false
+    }
+}
