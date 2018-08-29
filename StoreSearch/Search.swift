54630e12e871543ce4a4280c2071e6d69817cd1a
Mihaela Simion <mihaelasimion2016@gmail.com>
2018-08-29 12:31:09 +0300

    Refactor: improve the categories using enums

--- StoreSearch/Search.swift
+++ StoreSearch/Search.swift
@@ -11,40 +11,58 @@ import Foundation
 typealias SearchComplete = (Bool) -> Void
 
 class Search {
-    var searchResults: [SearchResult] = []
-    var hasSearched = false
-    var isLoading = false
-    
     private var dataTask: URLSessionDataTask? = nil
+    private(set) var state: State = .notSearchedYet
     
-    func performSearch(for text: String, category: Int, completion: @escaping SearchComplete) {
+    enum State {
+        case notSearchedYet
+        case loading
+        case noResults
+        case results([SearchResult])
+    }
+    
+    enum Category: Int {
+        case all = 0
+        case music = 1
+        case software = 2
+        case ebooks = 3
+        
+        var type: String {
+            switch self {
+            case .all: return ""
+            case .music: return "musicTrack"
+            case .software: return "software"
+            case .ebooks: return "ebook"
+            }
+        }
+    }
+    
+    func performSearch(for text: String, category: Category, completion: @escaping SearchComplete) {
         if !text.isEmpty {
             dataTask?.cancel()
-            isLoading = true
-            hasSearched = true
-            searchResults = []
+            state = .loading
             let url = iTunesURL(searchText: text, category: category)
             let session = URLSession.shared
             dataTask = session.dataTask(with: url) { (data, response, error) in
                 var success = false
+                var newState = State.notSearchedYet
                 if let error = error as NSError?, error.code == -999 {
                     return
                 } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200, let data = data {
-                    self.searchResults = self.parse(data: data)
-                    self.searchResults.sort(by: { (result1, result2) -> Bool in
-                        return result1.name < result2.name
-                    })
-                    print("Success")
-                    self.isLoading = false
+                    var searchResults = self.parse(data: data)
+                    if searchResults.isEmpty {
+                        newState = .noResults
+                    } else {
+                        searchResults.sort(by: { (result1, result2) -> Bool in
+                            return result1.name < result2.name
+                        })
+                        newState = .results(searchResults)
+                    }
                     success = true
                 }
                 
-                if !success {
-                    self.hasSearched = false
-                    self.isLoading = false
-                }
-                
                 DispatchQueue.main.async {
+                    self.state = newState
                     completion(success)
                 }
             }
@@ -52,15 +70,8 @@ class Search {
         }
     }
     
-    private func iTunesURL(searchText: String, category: Int) -> URL {
-        let kind: String
-        switch category {
-        case 1: kind = "musicTrack"
-        case 2: kind = "software"
-        case 3: kind = "ebook"
-        default: kind = ""
-        }
-        
+    private func iTunesURL(searchText: String, category: Category) -> URL {
+        let kind = category.type
         let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
         let urlString = "https://itunes.apple.com/search?" + "term=\(encodedText)&limit=200&entity=\(kind)"
         
