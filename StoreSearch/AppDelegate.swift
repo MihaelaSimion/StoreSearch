3fce059a9d84b44aa3eaee7c56dfb01dbee2c2c4
Mihaela Simion <mihaelasimion2016@gmail.com>
2018-08-31 10:04:02 +0300

    Add the popover

--- StoreSearch/AppDelegate.swift
+++ StoreSearch/AppDelegate.swift
@@ -30,6 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        splitVC.delegate = self
         customizeAppearance()
         detailVC.navigationItem.leftBarButtonItem = splitVC.displayModeButtonItem
         searchVC.splitViewDetail = detailVC
@@ -64,6 +65,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UISearchBar.appearance().barTintColor = barTintColor
         window!.tintColor = UIColor(red: 10/255, green: 80/255, blue: 80/255, alpha: 1)
     }
-
 }
+
+extension AppDelegate: UISplitViewControllerDelegate {
+    func splitViewController(_ svc: UISplitViewController, willChangeTo displayMode: UISplitViewControllerDisplayMode) {
+        print(#function)
+        if displayMode == .primaryOverlay {
+            svc.dismiss(animated: true, completion: nil)
+        }
+    }
+}
+
+
+
+
+
+
+
+
 
